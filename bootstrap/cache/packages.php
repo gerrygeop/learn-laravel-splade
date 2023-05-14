6298704f0a8a68787e69bfa3f290a97a54e9cff3
Gerry <45493466+gerrygeop@users.noreply.github.com>
2023-05-14 14:35:11 +0800

    bootstrap folder

---
+++ bootstrap/cache/packages.php
@@ -0,0 +1,108 @@
+<?php return array (
+  'laravel/dusk' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Dusk\\DuskServiceProvider',
+    ),
+  ),
+  'laravel/sail' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Sail\\SailServiceProvider',
+    ),
+  ),
+  'laravel/sanctum' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Sanctum\\SanctumServiceProvider',
+    ),
+  ),
+  'laravel/tinker' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Tinker\\TinkerServiceProvider',
+    ),
+  ),
+  'maatwebsite/excel' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Maatwebsite\\Excel\\ExcelServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Excel' => 'Maatwebsite\\Excel\\Facades\\Excel',
+    ),
+  ),
+  'nesbot/carbon' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Carbon\\Laravel\\ServiceProvider',
+    ),
+  ),
+  'nunomaduro/collision' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'NunoMaduro\\Collision\\Adapters\\Laravel\\CollisionServiceProvider',
+    ),
+  ),
+  'nunomaduro/termwind' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Termwind\\Laravel\\TermwindServiceProvider',
+    ),
+  ),
+  'protonemedia/laravel-dusk-fakes' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'ProtoneMedia\\LaravelDuskFakes\\LaravelDuskFakesServiceProvider',
+    ),
+  ),
+  'protonemedia/laravel-splade' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'ProtoneMedia\\Splade\\ServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Animation' => 'ProtoneMedia\\Splade\\Facades\\Animation',
+      'SEO' => 'ProtoneMedia\\Splade\\Facades\\SEO',
+      'Splade' => 'ProtoneMedia\\Splade\\Facades\\Splade',
+      'Toast' => 'ProtoneMedia\\Splade\\Facades\\Toast',
+    ),
+  ),
+  'protonemedia/laravel-splade-breeze' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Breeze\\BreezeServiceProvider',
+    ),
+  ),
+  'spatie/laravel-ignition' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Spatie\\LaravelIgnition\\IgnitionServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Flare' => 'Spatie\\LaravelIgnition\\Facades\\Flare',
+    ),
+  ),
+  'spatie/laravel-query-builder' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Spatie\\QueryBuilder\\QueryBuilderServiceProvider',
+    ),
+  ),
+);
